8bd8faf37a3bfea1353e3125aa6287aa0214e141
Subash <it.subashaa@gmail.com>
2021-12-30 13:15:59 +0530

    lines

--- js/canvas.js
+++ js/canvas.js
@@ -1,7 +1,7 @@
 // Variables
 const hueSpeed = 1;
-const moveParticles = 5;
-const clickParticles = 15;
+const moveParticles = 1;
+const clickParticles = 10;
 
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
@@ -42,7 +42,6 @@ class Particle {
   constructor() {
     this.x = mouse.x;
     this.y = mouse.y;
-
     this.size = Math.random() * 15 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
@@ -51,13 +50,32 @@ class Particle {
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
-    if (this.size > 0.2) this.size -= 0.1;
+    if (this.size > 0.2) this.size -= 0.2;
   }
   draw() {
     ctx.fillStyle = this.color;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
+  }
+}
+
+function connect() {
+  for (let a = 0; a < particlesArr.length; a++) {
+    for (let b = a; b < particlesArr.length; b++) {
+      let dx = particlesArr[a].x - particlesArr[b].x;
+      let dy = particlesArr[a].y - particlesArr[b].y;
+      let distance = dx * dx + dy * dy;
+
+      if (distance < (canvas.width / 7) * (canvas.height / 7)) {
+        ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+        ctx.lineWidth = 0.5;
+        ctx.beginPath();
+        ctx.moveTo(particlesArr[a].x, particlesArr[a].y);
+        ctx.lineTo(particlesArr[b].x, particlesArr[b].y);
+        ctx.stroke();
+      }
+    }
   }
 }
 
@@ -76,6 +94,7 @@ function animate() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticle();
+  connect();
   hue += hueSpeed;
   requestAnimationFrame(animate);
 }
